28b43674fae9207e1f1642e3de7fb59e482bab48
dxn-market-bilal <BilalJamalJassim100@gmail.com>
2026-07-18 23:36:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 const button = document.getElementById('myBtn');
 const inputName = document.getElementById('userName');
-const inputPhone = document.getElementById('userPhone'); // صيّحنا لحقل الهاتف الجديد
+const inputPhone = document.getElementById('userPhone');
+const selectCity = document.getElementById('userCity'); // صيّحنا للقائمة المنسدلة
 const messageText = document.getElementById('welcomeMessage');
 
-// حط رابط السيرفر الخاص بيك هنا
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyiDtaNjmpA7Q_qEVvAgKabvR9EWAmreLfv-P3LvcVlTSus0en1D8V0jUk5Nbhb4QHQxw/exec';
 
 button.addEventListener('click', function() {
     const name = inputName.value;
-    const phone = inputPhone.value; // سحبنا الرقم اللّي كتبه المستخدم
+    const phone = inputPhone.value;
+    const city = selectCity.value; // سحبنا الخيار اللّي اختاره المستخدم
 
-    // التأكد من إن الحقلين مو فارغات
-    if (name === "" || phone === "") {
-        messageText.innerText = "الرجاء ملء جميع الحقول أولاً! ⚠️";
+    // التأكد من إن كل الحقول ممتلئة والقائمة تم الاختيار منها
+    if (name === "" || phone === "" || city === "") {
+        messageText.innerText = "الرجاء ملء جميع الحقول واختيار المدينة! ⚠️";
         messageText.style.color = "red";
     } else {
-        messageText.innerText = "جاري إرسال بياناتك وحفظها... ⏳";
+        messageText.innerText = "جاري إرسال بياناتك... ⏳";
         messageText.style.color = "yellow";
 
-        // دمجنا الاسم ورقم الهاتف سوا بداخل رابط الإرسال
-        fetch(scriptURL + "?userName=" + encodeURIComponent(name) + "&userPhone=" + encodeURIComponent(phone), { method: 'POST' })
+        // أضفنا المدينة &userCity بداخل رابط الإرسال
+        fetch(scriptURL + "?userName=" + encodeURIComponent(name) + "&userPhone=" + encodeURIComponent(phone) + "&userCity=" + encodeURIComponent(city), { method: 'POST' })
         .then(response => {
-            messageText.innerText = "تم حفظ بياناتك بنجاح في الجدول! 😎🔥";
+            messageText.innerText = "تم حفظ بياناتك واختيار مدينتك بنجاح! 😎🔥";
             messageText.style.color = "#00ff88";
-            inputName.value = ""; // تفريغ حقل الاسم
-            inputPhone.value = ""; // تفريغ حقل الهاتف
+            inputName.value = ""; 
+            inputPhone.value = ""; 
+            selectCity.value = ""; // إعادة القائمة للوضع الافتراضي
         })
         .catch(error => {
             messageText.innerText = "عذراً، حدث خطأ في الاتصال! ❌";
